628c61b9eeb0945e53e531391856cbc29350042a
Your Name <you@example.com>
2024-01-08 02:29:43 -0800

    create Min class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Menu {
+
+  Scanner scanner = new Scanner(System.in);
+
+    public void publicMenu() {
+        System.out.println("***** WELCOME *****");
+        System.out.println("1-SIGN UP");
+        System.out.println("2-SIGN IN");
+        System.out.println("CHOSE YOUR NUMBER:");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }
