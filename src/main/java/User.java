e1f981e47ce54ff49dd8690f0347a557d31ee98b
Your Name <you@example.com>
2024-01-08 02:10:42 -0800

    create AND add identity

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,64 @@
 public class User {
-    public static void main(String[] args) {
-        
+
+
+
+        private String firsname;
+        private String lastname;
+        private String username;
+        private String password;
+
+    public User() {
+        }
+
+    public User(String firsname, String lastname, String username, String password) {
+            this.firsname = firsname;
+            this.lastname = lastname;
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getFirsname () {
+            return firsname;
+        }
+
+        public void setFirsname (String firsname){
+            this.firsname = firsname;
+        }
+
+        public String getLastname () {
+            return lastname;
+        }
+
+        public void setLastname (String lastname){
+            this.lastname = lastname;
+        }
+
+        public String getUsername () {
+            return username;
+        }
+
+        public void setUsername (String username){
+            this.username = username;
+        }
+
+        public String getPassword () {
+            return password;
+        }
+
+        public void setPassword (String password){
+            this.password = password;
+        }
+
+        @Override
+        public String toString () {
+            return "User{" +
+                    "firsname='" + firsname + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
     }
 
-}
+
+
